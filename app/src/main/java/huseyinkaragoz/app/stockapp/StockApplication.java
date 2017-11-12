e94c974bd78e3e56c240c0a04543476af27c54e5
huseyinkaragoz <huseyn.karagozz@gmail.com>
2017-11-12 20:30:20 +0300

    Fifth one

--- app/src/main/java/huseyinkaragoz/app/stockapp/StockApplication.java
+++ app/src/main/java/huseyinkaragoz/app/stockapp/StockApplication.java
@@ -5,11 +5,9 @@ import android.app.Application;
 
 import javax.inject.Inject;
 
-import dagger.Provides;
 import dagger.android.AndroidInjector;
 import dagger.android.DispatchingAndroidInjector;
 import dagger.android.HasActivityInjector;
-import huseyinkaragoz.app.stockapp.di.AppComponent;
 
 /**
  * Created by huseyinkaragozz on 25.10.2017.
